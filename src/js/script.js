ed1f93f1b5d40d6bf93212a5848348a67ff10b61
Natalya <notka25022000@mail.ru>
2023-02-03 09:38:51 +0300

    fourth

--- src/js/script.js
+++ src/js/script.js
@@ -21,6 +21,15 @@ $(document).ready(function(){
         speed: 1200,
         adaptiveHeight: true,
         prevArrow: '<button type="button" class="slick-prev"><img src="src/icons/left_arrow.png"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="src/icons/right_arrow.png"></button>'
+        nextArrow: '<button type="button" class="slick-next"><img src="src/icons/right_arrow.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }   
+            }
+        ]
     });
   }); 
